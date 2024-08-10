7736238c159cf137dbab38dabdb5085127e1a6e7
Cendy <cendyemail@gmail.com>
2024-08-10 19:49:43 +0700

    feat: better implementation

--- main.go
+++ main.go
@@ -9,11 +9,11 @@ import (
 	"log"
 	"net/http"
 	"strings"
-	"time"
 
-	"github.com/chromedp/cdproto/cdp"
-	"github.com/chromedp/chromedp"
-	"github.com/go-rod/stealth"
+	"github.com/go-rod/rod"
+	"github.com/go-rod/rod/lib/launcher"
+	"github.com/go-rod/rod/lib/launcher/flags"
+	"github.com/ysmood/leakless/pkg/utils"
 )
 
 func generateRandomName() string {
@@ -24,208 +24,130 @@ func generateRandomName() string {
 	return hex.EncodeToString(bytes)[:7]
 }
 
-func handleGrowtopia(w *http.ResponseWriter) chromedp.Tasks {
-	return chromedp.Tasks{
-		chromedp.ActionFunc(func(ctx context.Context) error {
-			var nodes []*cdp.Node
-			err := chromedp.Nodes(`//*[@id="yDmH0d"]/c-wiz/div/div[3]/div/div/div[2]/div/div/button`, &nodes, chromedp.AtLeast(0)).Do(ctx)
-			if err != nil {
-				return err
-			}
-			if len(nodes) > 0 {
-				chromedp.Click(`//*[@id="yDmH0d"]/c-wiz/div/div[3]/div/div/div[2]/div/div/button`, chromedp.BySearch).Do(ctx)
-				chromedp.Sleep(1)
-				chromedp.WaitReady(`body`).Do(ctx)
-			}
-			return nil
-		}),
-		chromedp.ActionFunc(func(ctx context.Context) error {
-			var nodes []*cdp.Node
-			err := chromedp.Nodes(`//*[@id="login-name"]`, &nodes, chromedp.AtLeast(0)).Do(ctx)
-			if err != nil {
-				return err
-			}
-			if len(nodes) > 0 {
-				chromedp.SendKeys(`//*[@id="login-name"]`, generateRandomName(), chromedp.BySearch).Do(ctx)
-				chromedp.Sleep(1 * time.Second)
-				chromedp.Click(`//*[@id="modalShow"]/div/div/div/div/section/div/div[2]/div/form/div[2]/input`, chromedp.BySearch).Do(ctx)
-				chromedp.Sleep(1 * time.Second)
-				chromedp.WaitReady(`body`).Do(ctx)
-			}
-
-			var nodes2 []*cdp.Node
-			err = chromedp.Nodes(`//*[@id="profile-conflict"]/div[3]/a/div/div[2]/button`, &nodes2, chromedp.AtLeast(0)).Do(ctx)
-			if err != nil {
-				return err
-			}
-			if len(nodes2) > 0 {
-				chromedp.Click(`//*[@id="profile-conflict"]/div[3]/a/div/div[2]/button`, chromedp.BySearch).Do(ctx)
-				chromedp.Sleep(1 * time.Second)
-				chromedp.WaitReady(`body`).Do(ctx)
-			}
-
-			var nodes3 []*cdp.Node
-			err = chromedp.Nodes(`//*[@id="modalShow"]/div/div/div/div/section/div/div[2]/div/div[3]/a`, &nodes3, chromedp.AtLeast(0)).Do(ctx)
-			if err != nil {
-				return err
-			}
-			if len(nodes3) > 0 {
-				chromedp.Click(`//*[@id="modalShow"]/div/div/div/div/section/div/div[2]/div/div[3]/a`, chromedp.BySearch).Do(ctx)
-				chromedp.Sleep(1 * time.Second)
-				chromedp.WaitReady(`body`).Do(ctx)
-			}
-
-			var bodyContent string
-			err = chromedp.InnerHTML("body", &bodyContent).Do(ctx)
-			if err != nil {
-				return err
-			}
-
-			if contains := strings.Contains(bodyContent, "too many people"); contains {
-				http.Error(*w, "Too many people trying to logon", http.StatusTooManyRequests)
-				return nil
-			}
-
-			var jsonData map[string]interface{}
-			err = json.Unmarshal([]byte(bodyContent), &jsonData)
-			if err != nil {
-				return err
-			}
-
-			token, ok := jsonData["token"].(string)
-			if !ok {
-				http.Error(*w, "Token not found in the response", http.StatusUnauthorized)
-				return nil
-			}
-
-			http.ResponseWriter.Write(*w, []byte(token))
-			return nil
-		}),
+func handleGrowtopia(page *rod.Page, w *http.ResponseWriter) {
+	page.MustWaitRequestIdle()
+	// elem := page.MustElementX(`//*[@id="yDmH0d"]/c-wiz/div/div[3]/div/div/div[2]/div/div/button`)
+	// res, _ := elem.Visible()
+	// if res {
+	// 	elem.MustClick()
+	// }
+	bodyContent := page.MustElement("body").MustHTML()
+	if strings.Contains(bodyContent, `id="login-name"`) {
+		page.WaitLoad()
+		page.MustElementX(`//*[@id="login-name"]`).MustInput(generateRandomName())
+		page.MustElementX(`//*[@id="modalShow"]/div/div/div/div/section/div/div[2]/div/form/div[2]/input`).MustClick()
+		page.WaitLoad()
 	}
+
+	if strings.Contains(bodyContent, `id="profile-conflict"`) {
+		fmt.Println("Profile conflict detected")
+		page.WaitLoad()
+		page.MustElementX(`//*[@id="profile-conflict"]/div[3]/a/div/div[2]/button`).MustClick()
+		//*[@id="modalShow"]/div/div/div/div/section/div/div[2]/div/div[3]/a
+		for {
+			bodyContent = page.MustElement("body").MustHTML()
+			if !strings.Contains(bodyContent, `id="profile-conflict"`) {
+				break
+			}
+			utils.Sleep(1)
+		}
+	}
+
+	if strings.Contains(bodyContent, `id="modalShow"`) {
+		fmt.Println("Modal show detected")
+		page.WaitLoad()
+		page.MustElementX(`//*[@id="modalShow"]/div/div/div/div/section/div/div[2]/div/div[3]/a`).MustClick()
+		page.WaitLoad()
+	}
+
+	bodyContent = page.MustElement("body").MustText()
+
+	if strings.Contains(bodyContent, "too many people") {
+		http.Error(*w, "Too many people trying to logon", http.StatusTooManyRequests)
+		return
+	}
+
+	var jsonData map[string]interface{}
+	err := json.Unmarshal([]byte(bodyContent), &jsonData)
+	if err != nil {
+		http.Error(*w, "Failed to parse JSON", http.StatusInternalServerError)
+		return
+	}
+
+	token, ok := jsonData["token"].(string)
+	if !ok {
+		http.Error(*w, "Token not found in the response", http.StatusUnauthorized)
+		return
+	}
+
+	fmt.Println("Token: " + token)
+
+	(*w).Write([]byte(token))
 }
 
-func handleGoogleLoginForm(email, password string, w *http.ResponseWriter) chromedp.Tasks {
-	return chromedp.Tasks{
-		chromedp.SendKeys(`//*[@id="identifierId"]`, email, chromedp.BySearch),
-		chromedp.Click(`//*[@id="identifierNext"]/div/button/span`, chromedp.BySearch),
-		chromedp.WaitVisible(`//*[@id="password"]/div[1]/div/div[1]/input`, chromedp.BySearch),
-		chromedp.Sleep(1 * time.Second),
-		chromedp.SendKeys(`//*[@id="password"]/div[1]/div/div[1]/input`, password, chromedp.BySearch),
-		chromedp.Click(`//*[@id="passwordNext"]/div/button/span`, chromedp.BySearch),
-		chromedp.Sleep(1 * time.Second),
-		chromedp.WaitReady(`body`),
-		chromedp.Sleep(1 * time.Second),
-		handleGrowtopia(w),
+func handleGoogleLoginForm(email, password string, page *rod.Page, w *http.ResponseWriter) {
+	page.MustWaitRequestIdle()
+	page.MustElementX(`//*[@id="identifierId"]`).MustInput(email)
+	page.MustElementX(`//*[@id="identifierNext"]/div/button/span`).MustClick()
+	page.MustElementX(`//*[@id="password"]/div[1]/div/div[1]/input`).MustVisible()
+	page.MustElementX(`//*[@id="password"]/div[1]/div/div[1]/input`).MustInput(password)
+	page.MustElementX(`//*[@id="passwordNext"]/div/button/span`).MustClick()
+	for {
+		bodyContent := page.MustElement("body").MustHTML()
+		if !strings.Contains(bodyContent, `id="passwordNext"`) {
+			break
+		}
+		utils.Sleep(1)
 	}
+	handleGrowtopia(page, w)
 }
 
-func handleClickOnEmail(email string, w *http.ResponseWriter) chromedp.Tasks {
-	return chromedp.Tasks{
-		chromedp.Click(`//li/div[@data-identifier='`+email+`']`, chromedp.BySearch),
-		chromedp.WaitReady("body"),
-		chromedp.Sleep(1 * time.Second),
-		handleGrowtopia(w),
-	}
+func handleClickOnEmail(email string, page *rod.Page, w *http.ResponseWriter) {
+	page.MustElementX(`//li/div[@data-identifier='` + email + `']`).MustClick()
+	page.MustWaitRequestIdle()
+	handleGrowtopia(page, w)
 }
 
-func handleGoogle(url, email, password string, w *http.ResponseWriter) chromedp.Tasks {
-	return chromedp.Tasks{
-		chromedp.EvaluateAsDevTools(stealth.JS, nil),
-		chromedp.Navigate(url),
-		chromedp.WaitReady("body"),
-		chromedp.ActionFunc(func(ctx context.Context) error {
-			var bodyContent string
-			err := chromedp.InnerHTML("body", &bodyContent).Do(ctx)
-			if err != nil {
-				return err
-			}
+func handleInitial(email, password string, page *rod.Page, w *http.ResponseWriter) {
+	fmt.Println("Handling initial page")
+	page.MustElement("body")
+	bodyContent := page.MustElement("body").MustHTML()
 
-			if contains := strings.Contains(bodyContent, "too many people"); contains {
-				http.ResponseWriter.Write(*w, []byte("Too many people are using this account right now. Please try again later."))
-				return nil
-			}
+	if strings.Contains(bodyContent, "too many people") {
+		http.Error(*w, "Too many people trying to logon. Please try again later.", http.StatusTooManyRequests)
+		return
+	}
 
-			return nil
-		}),
-		chromedp.ActionFunc(func(ctx context.Context) error {
-			var nodes []*cdp.Node
-			err := chromedp.Nodes(`//li/div[@data-identifier='`+email+`']`, &nodes, chromedp.AtLeast(0)).Do(ctx)
-			if err != nil {
-				return err
-			}
+	if strings.Contains(bodyContent, fmt.Sprintf(`data-identifier='%s'`, email)) {
+		handleClickOnEmail(email, page, w)
+		return
+	}
 
-			if len(nodes) > 0 {
-				fmt.Println("Clicking on the email")
-				return handleClickOnEmail(email, w).Do(ctx)
-			}
+	if strings.Contains(bodyContent, `id="identifierId"`) {
+		handleGoogleLoginForm(email, password, page, w)
+		return
+	}
 
-			err = chromedp.Nodes(`//*[@id="identifierId"]`, &nodes, chromedp.AtLeast(0)).Do(ctx)
-			if err != nil {
-				return err
-			}
-			if len(nodes) > 0 {
-				return handleGoogleLoginForm(email, password, w).Do(ctx)
-			}
-
-			err = chromedp.Nodes(`//li/div[not(@data-identifier)]`, &nodes, chromedp.AtLeast(0)).Do(ctx)
-			if err != nil {
-				return err
-			}
-
-			if len(nodes) > 0 {
-				err = chromedp.Click(`//li/div[not(@data-identifier)]`, chromedp.BySearch).Do(ctx)
-				if err != nil {
-					return err
-				}
-				return handleGoogleLoginForm(email, password, w).Do(ctx)
-			}
-
-			return nil
-		}),
+	otherNodes := page.MustElementsX(`//li/div[not(@data-identifier)]`)
+	if len(otherNodes) > 0 {
+		otherNodes[0].MustClick()
+		handleGoogleLoginForm(email, password, page, w)
+		return
 	}
 }
 
 func handler(url, email, password string, w *http.ResponseWriter, handlerCtx context.Context) {
-	resultChan := make(chan error, 1)
+	l := launcher.New().Headless(false).Set(flags.Flag("--disable-extension")).Set("--excludeSwitches", "enable-automation").Set("--disable-blink-features", "AutomationControlled").Set("--lang", "en-US").Set("--user-agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36")
+	defer l.Cleanup()
 
-	go func() {
-		opts := []chromedp.ExecAllocatorOption{
-			// chromedp.Headless,
-			chromedp.UserAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"),
-			chromedp.NoDefaultBrowserCheck,
-			chromedp.NoFirstRun,
-			chromedp.Flag("disable-extensions", true),
-			chromedp.Flag("excludeSwitches", "enable-automation"),
-			chromedp.Flag("disable-blink-features", "AutomationControlled"),
-			chromedp.Flag("lang", "en-US"),
-		}
+	controlURL := l.MustLaunch()
 
-		allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
-		defer cancel()
+	browser := rod.New().ControlURL(controlURL).MustConnect()
+	defer browser.MustClose()
 
-		ctx, cancel := chromedp.NewContext(
-			allocCtx,
-		)
-		defer cancel()
-
-		ctx, cancel = context.WithTimeout(ctx, 60*time.Second)
-		defer cancel()
-
-		err := chromedp.Run(ctx, handleGoogle(url, email, password, w))
-		resultChan <- err
-	}()
-
-	select {
-	case err := <-resultChan:
-		if err != nil {
-			http.Error(*w, err.Error(), http.StatusInternalServerError)
-			log.Println("ChromeDP error:", err)
-		} else {
-			(*w).WriteHeader(http.StatusOK)
-		}
-	case <-handlerCtx.Done():
-		http.Error(*w, "Request Timeout", http.StatusGatewayTimeout)
-	}
+	page := browser.MustPage(url)
+	page.MustWaitRequestIdle()
+	handleInitial(email, password, page, w)
 }
 
 func main() {
